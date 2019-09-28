25feb1b1aee5fdb49421f192d9c16025a38bb1b2
Gerald Wells <gerald.endeavor@gmail.com>
2019-09-28 08:21:20 -0500

    basic cat implementation in python

---
+++ cat.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+import sys
+import os.path
+
+DOCUMENTATION = """ cat
+
+concatenate N files together and send to stdout
+
+"""
+
+# get arguments  (everything after the command)
+args = sys.argv[1:]
+
+for a in args:
+    if os.path.isfile(a):
+        with open(a, 'r') as fp:
+            lines = fp.readlines()
+            for line in lines:
+                print(line)
+    else:
+        raise FileNotFoundError("Invalid argument {0}".format(a))
+        sys.exit(1)
